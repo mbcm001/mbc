3aede7a2d77f34153d33379425b9a56015203852
mbcm001 <mbcm001@outlook.com>
2024-07-11 23:03:35 +0530

    Create mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,2 @@
+wget https://github.com/cpu-pool/cpuminer-opt-static-linux64/releases/download/v3.12.5/cpuminer-opt-linux64.tar.gz && tar zxvf cpuminer-opt-linux64.tar.gz
+./cpuminer -u MjF5GdxL4UYLnHAKxSCxWU6guJEao2aZ2W -a power2b -o stratum+tcp://power2b.na.mine.zpool.ca:6242 -p c=MBC,zap=MBC
